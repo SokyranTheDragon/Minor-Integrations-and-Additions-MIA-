fb629a91652540f19ba8f32841a84e92c1c886bc
exploder1531 <exploder1531@gmail.com>
2019-11-25 11:53:13 +0100

    Added missing harvestcraft crops to thermal Phytogenic Insolator, more
    Registered missing tree saplings to oredict, hardened leather armor recycling

--- src/main/java/com/github/exploder1531/mia/integrations/harvestcraft/Harvestcraft.java
+++ src/main/java/com/github/exploder1531/mia/integrations/harvestcraft/Harvestcraft.java
@@ -4,6 +4,9 @@ import com.github.exploder1531.mia.integrations.ModIds;
 import com.github.exploder1531.mia.integrations.ModLoadStatus;
 import com.github.exploder1531.mia.integrations.base.IBaseMod;
 import com.github.exploder1531.mia.integrations.base.IModIntegration;
+import com.pam.harvestcraft.blocks.FruitRegistry;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.oredict.OreDictionary;
 
 import java.util.function.BiConsumer;
 
@@ -16,5 +19,13 @@ public class Harvestcraft implements IBaseMod
             modIntegration.accept(ModIds.THERMAL_EXPANSION, new ThermalExpansionHarvestcraftIntegration());
         if (ModLoadStatus.jerLoaded)
             modIntegration.accept(ModIds.JER, new JerHarvestcraftIntegration());
+    }
+    
+    @Override
+    public void init(FMLInitializationEvent event)
+    {
+        OreDictionary.registerOre("treeSapling", FruitRegistry.getSapling(FruitRegistry.SPIDERWEB));
+        OreDictionary.registerOre("treeSapling", FruitRegistry.getSapling(FruitRegistry.AVOCADO));
+        OreDictionary.registerOre("treeSapling", FruitRegistry.getSapling(FruitRegistry.WALNUT));
     }
 }
