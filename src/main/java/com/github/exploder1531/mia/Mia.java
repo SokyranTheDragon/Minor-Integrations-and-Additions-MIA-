eb963b9c7192b7b185936b5ee2b42af38af0a3bf
exploder1531 <exploder1531@gmail.com>
2020-01-12 23:22:00 +0100

    Updated mod version to 0.1.0a, as the first version

--- src/main/java/com/github/exploder1531/mia/Mia.java
+++ src/main/java/com/github/exploder1531/mia/Mia.java
@@ -44,7 +44,7 @@ public class Mia
 {
     public static final String MODID = "mia";
     public static final String NAME = "Minor Integrations & Additions";
-    public static final String VERSION = "1.12.2-0.1.5";
+    public static final String VERSION = "1.12.2-0.1.0a";
     
     @Mod.Instance
     public static Mia instance;
