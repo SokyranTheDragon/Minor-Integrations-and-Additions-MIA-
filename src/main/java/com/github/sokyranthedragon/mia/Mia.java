a76525d6f025658fe6c909cf6c78fd4d489032ee
SokyranTheDragon <exploder1531@gmail.com>
2020-02-15 03:47:49 +0100

    Bumped up the version

--- src/main/java/com/github/sokyranthedragon/mia/Mia.java
+++ src/main/java/com/github/sokyranthedragon/mia/Mia.java
@@ -48,7 +48,7 @@ public class Mia
 {
     public static final String MODID = "mia";
     public static final String NAME = "Minor Integrations & Additions";
-    public static final String VERSION = "1.12.2-0.1.4b";
+    public static final String VERSION = "1.12.2-0.1.5a";
     
     @Mod.Instance
     public static Mia instance;
