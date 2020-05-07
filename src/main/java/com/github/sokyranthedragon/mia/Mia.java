a471f1e7f4f933588f466116b935ffc6fd6b6648
SokyranTheDragon <exploder1531@gmail.com>
2020-05-08 01:55:52 +0200

    Made I&F 1.9.0 the minimum required version, updated mod number

--- src/main/java/com/github/sokyranthedragon/mia/Mia.java
+++ src/main/java/com/github/sokyranthedragon/mia/Mia.java
@@ -28,7 +28,7 @@ import org.apache.logging.log4j.Logger;
                 ";after:" + ModIds.ConstantIds.TINKERS_CONSTRUCT +
                 ";after:" + ModIds.ConstantIds.JEI +
                 ";after:" + ModIds.ConstantIds.JER + "@[0.9.1.56,]" +
-                ";after:" + ModIds.ConstantIds.ICE_AND_FIRE +
+                ";after:" + ModIds.ConstantIds.ICE_AND_FIRE + "@[1.9.0,]" +
                 ";after:" + ModIds.ConstantIds.HATCHERY +
                 ";after:" + ModIds.ConstantIds.THAUMCRAFT +
                 ";after:" + ModIds.ConstantIds.MO_CREATURES +
@@ -51,7 +51,7 @@ public class Mia
 {
     public static final String MODID = "mia";
     public static final String NAME = "Minor Integrations & Additions";
-    public static final String VERSION = "1.12.2-0.1.6a";
+    public static final String VERSION = "1.12.2-0.1.6b";
     
     @Mod.Instance
     public static Mia instance;
