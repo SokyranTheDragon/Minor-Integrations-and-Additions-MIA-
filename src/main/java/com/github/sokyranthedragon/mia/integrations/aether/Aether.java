51da27102295f08940d590f3422fc20175b36be1
SokyranTheDragon <exploder1531@gmail.com>
2020-08-02 16:41:31 +0200

    Added a handful of new aspects

--- src/main/java/com/github/sokyranthedragon/mia/integrations/aether/Aether.java
+++ src/main/java/com/github/sokyranthedragon/mia/integrations/aether/Aether.java
@@ -6,9 +6,15 @@ import com.github.sokyranthedragon.mia.integrations.base.IBaseMod;
 import com.github.sokyranthedragon.mia.integrations.base.IModIntegration;
 import com.legacy.aether.blocks.BlocksAether;
 import com.legacy.aether.items.ItemsAether;
+import net.minecraft.block.Block;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import thaumcraft.api.aspects.*;
 
+import java.util.Map;
 import java.util.function.BiConsumer;
 
 import static com.github.sokyranthedragon.mia.config.AetherConfig.*;
@@ -54,9 +60,65 @@ public class Aether implements IBaseMod
             registerOre("plankWood", new ItemStack(BlocksAether.skyroot_plank));
             registerOre("grass", new ItemStack(BlocksAether.aether_grass));
             registerOre("dirt", new ItemStack(BlocksAether.aether_dirt));
+            registerOre("blockMossy", new ItemStack(BlocksAether.mossy_holystone));
             
             registerOre("listAllBerry", new ItemStack(ItemsAether.blue_berry));
             registerOre("listAllFruit", new ItemStack(ItemsAether.blue_berry));
         }
+    }
+    
+    @Override
+    public void registerAspects(AspectRegistryEvent event)
+    {
+        AspectEventProxy register = event.register;
+        
+        register.registerObjectTag(new ItemStack(BlocksAether.enchanted_aether_grass), new AspectList().add(Aspect.EARTH, 5).add(Aspect.PLANT, 2).add(Aspect.MAGIC, 2));
+        register.registerObjectTag(new ItemStack(BlocksAether.holystone), new AspectList().add(Aspect.EARTH, 5));
+        register.registerObjectTag(new ItemStack(BlocksAether.holystone), new AspectList().add(Aspect.EARTH, 5).add(Aspect.PLANT, 3).add(Aspect.ENTROPY, 1));
+        register.registerObjectTag(new ItemStack(BlocksAether.icestone), new AspectList().add(Aspect.COLD, 5));
+        
+        register.registerObjectTag(new ItemStack(ItemsAether.zanite_gemstone), new AspectList().add(Aspect.DESIRE, 15).add(Aspect.CRYSTAL, 15).add(Aspect.MAGIC, 1));
+        
+        AspectList list = new AspectList().add(Aspect.PLANT, 5).add(Aspect.SENSES, 5).add(Aspect.LIFE, 1);
+        register.registerObjectTag(new ItemStack(BlocksAether.purple_flower), list);
+        register.registerObjectTag(new ItemStack(BlocksAether.white_flower), list);
+        
+        AspectList tempList = AspectHelper.getObjectAspects(new ItemStack(Items.CHAINMAIL_CHESTPLATE));
+        list = new AspectList();
+        for (Map.Entry<Aspect, Integer> entries : tempList.aspects.entrySet())
+        {
+            if (entries.getValue() / 2 > 0)
+                list.add(entries.getKey(), entries.getValue());
+        }
+        register.registerObjectTag(new ItemStack(ItemsAether.chain_gloves), list);
+        
+        for (Item item : ItemsAether.itemRegistry)
+        {
+            ItemStack stack = new ItemStack(item);
+            if (item.isDamageable() || item.getMaxDamage(stack) == 0)
+                addAirAspect(stack, register);
+            else
+            {
+                for (int i = 0; i < item.getMaxDamage(stack); i++)
+                    addAirAspect(stack, register);
+            }
+        }
+        
+        for (Block block : BlocksAether.blockList)
+        {
+            NonNullList<ItemStack> stacks = NonNullList.create();
+            block.getSubBlocks(null, stacks);
+            
+            for (ItemStack stack : stacks)
+                addAirAspect(stack, register);
+        }
+    }
+    
+    private static void addAirAspect(ItemStack stack, AspectEventProxy register)
+    {
+        AspectList aspects = AspectHelper.getObjectAspects(stack);
+        
+        if (aspects.getAmount(Aspect.AIR) == 0)
+            register.registerObjectTag(stack, aspects.add(Aspect.AIR, 1));
     }
 }
--- src/main/java/com/github/sokyranthedragon/mia/integrations/mia/MiaSelfIntegrations.java
+++ src/main/java/com/github/sokyranthedragon/mia/integrations/mia/MiaSelfIntegrations.java
@@ -1,8 +1,16 @@
 package com.github.sokyranthedragon.mia.integrations.mia;
 
+import com.github.sokyranthedragon.mia.core.MiaBlocks;
+import com.github.sokyranthedragon.mia.core.MiaItems;
 import com.github.sokyranthedragon.mia.integrations.ModIds;
 import com.github.sokyranthedragon.mia.integrations.base.IBaseMod;
 import com.github.sokyranthedragon.mia.integrations.base.IModIntegration;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Optional;
+import thaumcraft.api.aspects.*;
+import vazkii.botania.api.BotaniaAPI;
+import vazkii.botania.common.block.ModBlocks;
+import vazkii.botania.common.item.block.ItemBlockSpecialFlower;
 
 import java.util.function.BiConsumer;
 
@@ -15,5 +23,39 @@ public class MiaSelfIntegrations implements IBaseMod
     {
         if (TINKERS_CONSTRUCT.isLoaded)
             modIntegration.accept(TINKERS_CONSTRUCT, new MiaTConstructIntegration());
+    }
+    
+    @Override
+    @Optional.Method(modid = ModIds.ConstantIds.THAUMCRAFT)
+    public void registerAspects(AspectRegistryEvent event)
+    {
+        AspectEventProxy register = event.register;
+    
+        if (MiaItems.koboldRing != null)
+        {
+            ItemStack stack = new ItemStack(MiaItems.koboldRing);
+            AspectList aspects = AspectHelper.getObjectAspects(stack);
+    
+            Aspect draco = Aspect.getAspect("draco");
+            
+            if (draco != null)
+                aspects.add(draco, 2);
+            aspects.add(Aspect.MAGIC, 5);
+            
+            register.registerObjectTag(stack, aspects);
+        }
+        if (MiaBlocks.flowerDead != null)
+            register.registerObjectTag(new ItemStack(MiaBlocks.flowerDead), AspectHelper.getObjectAspects(new ItemStack(MiaBlocks.flowerDead)).add(Aspect.PLANT, 1).add(Aspect.FIRE, 1));
+        if (MiaBlocks.armoredGlass != null)
+            register.registerObjectTag(new ItemStack(MiaBlocks.armoredGlass), AspectHelper.getObjectAspects(new ItemStack(MiaBlocks.armoredGlass)).add(Aspect.PROTECT, 3));
+        if (MiaBlocks.eggSorter != null)
+            register.registerObjectTag(new ItemStack(MiaBlocks.eggSorter), AspectHelper.getObjectAspects(new ItemStack(MiaBlocks.eggSorter)).add(Aspect.PLANT, 18));
+        if (MiaBlocks.blockBotaniaSpecialFlower != null)
+        {
+            AspectList aspects = new AspectList().add(Aspect.PLANT, 15).add(Aspect.SENSES, 15).add(Aspect.DESIRE, 10).add(Aspect.METAL, 5).add(Aspect.VOID, 5).add(Aspect.MAGIC, 5);
+    
+            register.registerComplexObjectTag(ItemBlockSpecialFlower.ofType("orechidVacuam"), aspects);
+            register.registerComplexObjectTag(ItemBlockSpecialFlower.ofType(new ItemStack(ModBlocks.floatingSpecialFlower), "orechidVacuam"), aspects);
+        }
     }
 }
