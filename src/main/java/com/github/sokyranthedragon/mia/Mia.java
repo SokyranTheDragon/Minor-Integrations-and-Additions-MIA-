9149bff466e2fa8fe0327b37e96d5707a35715c8
SokyranTheDragon <exploder1531@gmail.com>
2020-09-06 15:53:36 +0200

    Updated mod version

--- src/main/java/com/github/sokyranthedragon/mia/Mia.java
+++ src/main/java/com/github/sokyranthedragon/mia/Mia.java
@@ -67,7 +67,7 @@ public class Mia
 {
     public static final String MODID = "mia";
     public static final String NAME = "Minor Integrations & Additions";
-    public static final String VERSION = "1.12.2-0.2.0b";
+    public static final String VERSION = "1.12.2-0.2.0c";
     
     @Mod.Instance
     public static Mia instance;
