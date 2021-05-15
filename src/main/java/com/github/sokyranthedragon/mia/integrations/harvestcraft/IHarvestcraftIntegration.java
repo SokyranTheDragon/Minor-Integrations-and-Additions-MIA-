268d44653df3ed653a05c04f1c8f4f90921bbb9a
SokyranTheDragon <exploder1531@gmail.com>
2021-05-15 02:42:10 +0200

    Removed the (basically unneccessary) AbyssalCraft integration

--- src/main/java/com/github/sokyranthedragon/mia/integrations/harvestcraft/IHarvestcraftIntegration.java
+++ src/main/java/com/github/sokyranthedragon/mia/integrations/harvestcraft/IHarvestcraftIntegration.java
@@ -4,10 +4,6 @@ import com.github.sokyranthedragon.mia.integrations.base.IModIntegration;
 
 public interface IHarvestcraftIntegration extends IModIntegration
 {
-    default void replaceFood()
-    {
-    }
-    
     default void addRecipes()
     {
     }
